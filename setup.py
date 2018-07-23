1fe2f3163de9cc264723e61eda4829dcaf52021f
Florian Golemo <fgolemo@gmail.com>
2018-07-23 13:51:40 +0200

    bumped the version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if IS_RPI:
 
 setup(
     name='duckietown_slimremote',
-    version='1.4.3',
+    version='1.4.4',
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
