d01e685788e07f5b26955a8c5b24f53aaa2daed6
lpaull <lpaull@mit.edu>
2018-11-27 22:31:21 -0500

    small fixes

--- tests/12-test-keyboard-controller.py
+++ tests/12-test-keyboard-controller.py
@@ -1,9 +1,13 @@
 import sys
 from duckietown_slimremote.pc.robot import KeyboardControlledRobot
 
-host = "duckmylife2.local"
-print ("connecting to host",host)
+
+
 if len(sys.argv) > 1:
     host = sys.argv[1]
-
+else:
+    print ("usage: python3 12-test-keyboard-controller.py ![hostname]")
+    exit(0)
+    
+print ("connecting to host",host)
 kbd = KeyboardControlledRobot(host)
