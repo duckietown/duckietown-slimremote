10d9e34da2805c3d29810d60f5a4cad834cdf71d
Florian Golemo <fgolemo@gmail.com>
2018-11-28 18:56:31 -0500

    fixed the wheel orientation to new duckiebot wiring

--- duckietown_slimremote/pc/robot.py
+++ duckietown_slimremote/pc/robot.py
@@ -9,12 +9,13 @@ from duckietown_slimremote.helpers import random_id
 from duckietown_slimremote.networking import make_push_socket, construct_action, RESET, host_is_reachable
 from duckietown_slimremote.pc.camera import SubCameraMaster
 
+
 # cd /workspace/src/duckietown-slimremote/ && git config --global user.email "you@example.com" && \
-# git config --global user.name "Your Name" && git pull origin testing && git reset --hard origin/testing
+# git config --global user.name "Your Name" && git pull origin testing; git reset --hard origin/testing
 
 
 class RemoteRobot:
-    def __init__(self, host, shape, dtype, silent=False):
+    def __init__(self, host, shape=(640, 480, 3), dtype=np.uint8, silent=False):
         logger.info('Slimremote trying to connect to "{}"'.format(host))
         reachable = host_is_reachable(host)
         logger.info("'ping' reports host is " + ("REACHABLE" if reachable else "NOT reachable"))
---
+++ tests/16-test-motor-commands-no-tk.py
@@ -0,0 +1,41 @@
+import sys
+import time
+
+from duckietown_slimremote.pc.robot import KeyboardControlledRobot, RemoteRobot
+
+host = "duckmylife3.local"
+
+FPS = 15
+
+if len(sys.argv) > 1:
+    host = sys.argv[1]
+# else:
+#     print("usage: python3 16-test-motor-commands-no-tk.py ![hostname]")
+#     exit(0)
+
+print("connecting to host", host)
+robot = RemoteRobot(host)
+
+# init
+robot.reset()  # this is a dummy function on the real robot
+robot.step([0, 0], with_observation=False)  # init socket if it isn't
+
+# go straight forward for one second
+
+print("straight")
+start = time.time()
+while time.time() - start < 1:
+    _ = robot.step([1, 1], with_observation=False)
+    obs, rew, done, misc = robot.observe()
+    time.sleep(1 / FPS)
+
+print("1s break")
+time.sleep(1)
+
+# go left and forward for one second
+print("left forward")
+start = time.time()
+while time.time() - start < 1:
+    _ = robot.step([.4, .7], with_observation=False)
+    obs, rew, done, misc = robot.observe()
+    time.sleep(1 / FPS)
